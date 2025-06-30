f73dd616578fe8f49827eb4a485bc3f672509faf
LilSarkay <Saanvi.ravikiran@gmail.com>
2025-06-30 16:01:26 +0530

    Fix: Set correct start script and port

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ mongoose.connect(process.env.MONGO_URI, {
 })
 .then(() => {
   // Start server only after successful DB connection
-  const PORT = process.env.PORT || 5000;
+  const PORT = process.env.PORT || 8080;
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
