9f7b7f7a810bf9f8df5aa66127af3f3df8de719e
LilSarkay <Saanvi.ravikiran@gmail.com>
2025-07-02 18:24:08 +0530

    changed the port

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ mongoose.connect(process.env.MONGO_URI, {
   useUnifiedTopology: true,
 })
 .then(() => {
-  const PORT = process.env.PORT || 8080;
+  const PORT = process.env.PORT || 10000;
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
